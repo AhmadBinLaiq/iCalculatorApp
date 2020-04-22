d910f95b25df2553d2f3e48b5a4a616af3f546b0
Ahmad <ahmadlaiq@outlook.com>
2020-04-22 21:18:45 +0500

    Initial Commit

---
+++ iCalculatorApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iCalculatorApp
+//
+//  Created by Ahmad Bin Laiq on 22/04/2020.
+//  Copyright © 2020 Ahmad Bin Laiq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
